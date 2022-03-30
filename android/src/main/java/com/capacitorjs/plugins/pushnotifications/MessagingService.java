4b5e9387a239b76c2613d60fbd436659e305e3b7
nkalupahana <avishkank@gmail.com>
2022-03-30 13:02:19 -0500

    update code

--- android/src/main/java/com/capacitorjs/plugins/pushnotifications/MessagingService.java
+++ android/src/main/java/com/capacitorjs/plugins/pushnotifications/MessagingService.java
@@ -3,7 +3,7 @@ package com.capacitorjs.plugins.pushnotifications;
 import android.app.NotificationManager;
 import android.service.notification.StatusBarNotification;
 
-import android.util.Log;
+import android.util.Pair;
 import androidx.annotation.NonNull;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -24,21 +24,21 @@ public class MessagingService extends FirebaseMessagingService {
                 if (remoteMessage.getData().containsKey("cleanUp")) {
                     // Find the latest notification in all delivered notifications
                     NotificationManager notificationManager = getSystemService(NotificationManager.class);
-                    ArrayList<Pair<String, Integer>> data = new ArrayList<>();
+                    ArrayList<Pair<String, Integer>> ids = new ArrayList<>();
                     StatusBarNotification latestNotif = null;
                     for (StatusBarNotification notif : notificationManager.getActiveNotifications()) {
-                        data.add(new Pair<String, Integer>(notif.getTag(), notif.getId()));
+                        ids.add(Pair.create(notif.getTag(), notif.getId()));
                         if (latestNotif == null || latestNotif.getPostTime() < notif.getPostTime()) {
                             latestNotif = notif;
                         }
                     }
 
                     // Remove the latest from the list of all delivered
-                    if (latestNotif != null) ids.remove(new Pair<String, Integer>(latestNotif.getTag(), latestNotif.getId()));
+                    if (latestNotif != null) ids.remove(Pair.create(latestNotif.getTag(), latestNotif.getId()));
 
                     // And remove all other notifications
                     for (Pair<String, Integer> pair : ids) {
-                        notificationManager.cancel(pair.getKey(), pair.getValue());
+                        notificationManager.cancel(pair.first, pair.second);
                     }
                 }
             }
